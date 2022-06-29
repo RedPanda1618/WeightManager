9457b055f3b274907c82cf0f368c7396ae46b849
redpanda <hawkflight2661@gmail.com>
2022-06-29 20:45:54 +0900

    Init Commit

---
+++ app/src/main/java/io/github/redpanda1618/weightmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.redpanda1618.weightmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
