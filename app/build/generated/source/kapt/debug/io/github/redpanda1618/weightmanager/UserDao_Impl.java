92afc8e3652cd5c1e3468f28398cbe016a0f9725
RedPanda1618 <shumpei116@gmail.com>
2022-06-29 23:58:47 +0900

    else

---
+++ app/build/generated/source/kapt/debug/io/github/redpanda1618/weightmanager/UserDao_Impl.java
@@ -0,0 +1,179 @@
+package io.github.redpanda1618.weightmanager;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class UserDao_Impl implements UserDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<User> __insertionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __deletionAdapterOfUser;
+
+  private final SharedSQLiteStatement __preparedStmtOfClear;
+
+  public UserDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfUser = new EntityInsertionAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `user_table` (`id`,`date`,`weight`,`muscle_rate`,`fat_rate`) VALUES (nullif(?, 0),?,?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        stmt.bindDouble(3, value.getAge());
+        stmt.bindDouble(4, value.getMuscle_rate());
+        stmt.bindDouble(5, value.getFat_rate());
+      }
+    };
+    this.__deletionAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `user_table` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        stmt.bindLong(1, value.getId());
+      }
+    };
+    this.__preparedStmtOfClear = new SharedSQLiteStatement(__db) {
+      @Override
+      public String createQuery() {
+        final String _query = "DELETE FROM user_table";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public Object insert(final User user, final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfUser.insert(user);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object delete(final User user, final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __deletionAdapterOfUser.handle(user);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object clear(final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        final SupportSQLiteStatement _stmt = __preparedStmtOfClear.acquire();
+        __db.beginTransaction();
+        try {
+          _stmt.executeUpdateDelete();
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+          __preparedStmtOfClear.release(_stmt);
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object get(final long key, final Continuation<? super User> continuation) {
+    final String _sql = "SELECT * FROM user_table WHERE id = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    _statement.bindLong(_argIndex, key);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<User>() {
+      @Override
+      public User call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "date");
+          final int _cursorIndexOfAge = CursorUtil.getColumnIndexOrThrow(_cursor, "weight");
+          final int _cursorIndexOfMuscleRate = CursorUtil.getColumnIndexOrThrow(_cursor, "muscle_rate");
+          final int _cursorIndexOfFatRate = CursorUtil.getColumnIndexOrThrow(_cursor, "fat_rate");
+          final User _result;
+          if(_cursor.moveToFirst()) {
+            final long _tmpId;
+            _tmpId = _cursor.getLong(_cursorIndexOfId);
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final float _tmpAge;
+            _tmpAge = _cursor.getFloat(_cursorIndexOfAge);
+            final float _tmpMuscle_rate;
+            _tmpMuscle_rate = _cursor.getFloat(_cursorIndexOfMuscleRate);
+            final float _tmpFat_rate;
+            _tmpFat_rate = _cursor.getFloat(_cursorIndexOfFatRate);
+            _result = new User(_tmpId,_tmpName,_tmpAge,_tmpMuscle_rate,_tmpFat_rate);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, continuation);
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/io/github/redpanda1618/weightmanager/UserDao.java
@@ -0,0 +1,30 @@
+package io.github.redpanda1618.weightmanager;
+
+import java.lang.System;
+
+@androidx.room.Dao()
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\t\n\u0002\b\u0003\bg\u0018\u00002\u00020\u0001J\u0011\u0010\u0002\u001a\u00020\u0003H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0004J\u0019\u0010\u0005\u001a\u00020\u00032\u0006\u0010\u0006\u001a\u00020\u0007H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\bJ\u001b\u0010\t\u001a\u0004\u0018\u00010\u00072\u0006\u0010\n\u001a\u00020\u000bH\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\fJ\u0019\u0010\r\u001a\u00020\u00032\u0006\u0010\u0006\u001a\u00020\u0007H\u00a7@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\b\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\u000e"}, d2 = {"Lio/github/redpanda1618/weightmanager/UserDao;", "", "clear", "", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "delete", "user", "Lio/github/redpanda1618/weightmanager/User;", "(Lio/github/redpanda1618/weightmanager/User;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "get", "key", "", "(JLkotlin/coroutines/Continuation;)Ljava/lang/Object;", "insert", "app_debug"})
+public abstract interface UserDao {
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Insert()
+    public abstract java.lang.Object insert(@org.jetbrains.annotations.NotNull()
+    io.github.redpanda1618.weightmanager.User user, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Delete()
+    public abstract java.lang.Object delete(@org.jetbrains.annotations.NotNull()
+    io.github.redpanda1618.weightmanager.User user, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "DELETE FROM user_table")
+    public abstract java.lang.Object clear(@org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation);
+    
+    @org.jetbrains.annotations.Nullable()
+    @androidx.room.Query(value = "SELECT * FROM user_table WHERE id = :key")
+    public abstract java.lang.Object get(long key, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super io.github.redpanda1618.weightmanager.User> continuation);
+}
