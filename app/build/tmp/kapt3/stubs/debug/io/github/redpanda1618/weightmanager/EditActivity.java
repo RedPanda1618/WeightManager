c5ac064b57690ed4f05d48ff6815daf75ea720c6
RedPanda1618 <shumpei116@gmail.com>
2022-07-23 00:17:40 +0900

    else

---
+++ app/build/tmp/kapt3/stubs/debug/io/github/redpanda1618/weightmanager/EditActivity.java
@@ -0,0 +1,73 @@
+package io.github.redpanda1618.weightmanager;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u00008\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u0000 \u00112\u00020\u0001:\u0001\u0011B\u0005\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006J$\u0010\u0007\u001a\u001e\u0012\u0014\u0012\u0012\u0012\u0004\u0012\u00020\n0\tj\b\u0012\u0004\u0012\u00020\n`\u000b\u0012\u0004\u0012\u00020\f0\bH\u0002J\u0012\u0010\r\u001a\u00020\u00042\b\u0010\u000e\u001a\u0004\u0018\u00010\u000fH\u0014J\b\u0010\u0010\u001a\u00020\u0004H\u0003\u00a8\u0006\u0012"}, d2 = {"Lio/github/redpanda1618/weightmanager/EditActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "clearData", "", "view", "Landroid/view/View;", "getData", "Lkotlin/Pair;", "Ljava/util/ArrayList;", "Lio/github/redpanda1618/weightmanager/User;", "Lkotlin/collections/ArrayList;", "", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "showTable", "Companion", "app_debug"})
+public final class EditActivity extends androidx.appcompat.app.AppCompatActivity {
+    @org.jetbrains.annotations.NotNull()
+    private static final io.github.redpanda1618.weightmanager.EditActivity.Companion Companion = null;
+    @android.annotation.SuppressLint(value = {"StaticFieldLeak"})
+    @java.lang.Deprecated()
+    public static android.widget.TextView textViewData;
+    @android.annotation.SuppressLint(value = {"StaticFieldLeak"})
+    @java.lang.Deprecated()
+    public static android.widget.TableLayout tableLayout;
+    @java.lang.Deprecated()
+    public static io.github.redpanda1618.weightmanager.UserDao dao;
+    
+    public EditActivity() {
+        super();
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    @android.annotation.SuppressLint(value = {"SetTextI18n"})
+    private final void showTable() {
+    }
+    
+    private final kotlin.Pair<java.util.ArrayList<io.github.redpanda1618.weightmanager.User>, java.lang.Integer> getData() {
+        return null;
+    }
+    
+    public final void clearData(@org.jetbrains.annotations.NotNull()
+    android.view.View view) {
+    }
+    
+    @kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\b\u0082\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001e\u0010\t\u001a\u00020\n8\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000b\u0010\f\"\u0004\b\r\u0010\u000eR\u001e\u0010\u000f\u001a\u00020\u00108\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\u0012\"\u0004\b\u0013\u0010\u0014\u00a8\u0006\u0015"}, d2 = {"Lio/github/redpanda1618/weightmanager/EditActivity$Companion;", "", "()V", "dao", "Lio/github/redpanda1618/weightmanager/UserDao;", "getDao", "()Lio/github/redpanda1618/weightmanager/UserDao;", "setDao", "(Lio/github/redpanda1618/weightmanager/UserDao;)V", "tableLayout", "Landroid/widget/TableLayout;", "getTableLayout", "()Landroid/widget/TableLayout;", "setTableLayout", "(Landroid/widget/TableLayout;)V", "textViewData", "Landroid/widget/TextView;", "getTextViewData", "()Landroid/widget/TextView;", "setTextViewData", "(Landroid/widget/TextView;)V", "app_debug"})
+    static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final android.widget.TextView getTextViewData() {
+            return null;
+        }
+        
+        public final void setTextViewData(@org.jetbrains.annotations.NotNull()
+        android.widget.TextView p0) {
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final android.widget.TableLayout getTableLayout() {
+            return null;
+        }
+        
+        public final void setTableLayout(@org.jetbrains.annotations.NotNull()
+        android.widget.TableLayout p0) {
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final io.github.redpanda1618.weightmanager.UserDao getDao() {
+            return null;
+        }
+        
+        public final void setDao(@org.jetbrains.annotations.NotNull()
+        io.github.redpanda1618.weightmanager.UserDao p0) {
+        }
+    }
+}
